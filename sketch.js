da717c24cb489532ade6b45691dbfa44b6446b37
Francisco Dias <frandias@gmail.com>
2016-01-23 14:26:46 -0500

    example of loops

--- sketch.js
+++ sketch.js
@@ -1,3 +1,22 @@
+/*
+  // A loop is structued like this:
+  keyword (condition) {
+    stuff that happens
+  }
+  
+  // while loop
+  // (condition)
+  while (keep going as long as this evaluates to true) {
+    // stuff happens
+  }
+  
+  // for loop:
+  // (variable assignment; condition to run while true; change to iterator)
+  for(var i = 0; i < 20; i = i+ 1) {
+    // stuff happens
+  }
+*/
+
 function setup() {
   
 }
